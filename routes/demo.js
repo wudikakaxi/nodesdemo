d6e67602d9267c78cfd3fda48f0001e961c36fa8
wudi <wu.di@etao.cn>
2016-11-16 19:09:00 +0800

    试验pm2

--- routes/demo.js
+++ routes/demo.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express1111111111111' });
 });
 
 router.get('/redirect',function(req,res,next){
